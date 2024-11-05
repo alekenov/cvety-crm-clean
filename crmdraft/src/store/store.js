6d17885a875ce7367788132645b1aa7a5b541dec
Chingis Alekenov <alekenov@gmail.com>
2024-11-05 12:23:56 +0500

    Исправлена структура проекта

--- crmdraft/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import clientsReducer from './slices/clientsSlice';
-
-export const store = configureStore({
-  reducer: {
-    clients: clientsReducer,
-  },
-}); 
